cbf892ce0ac327bdf8a51d7e370a028a0a495b07
ahnl <46541386+ahnl@users.noreply.github.com>
2020-11-17 17:02:57 +0200

    1.8

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,24 +1,9 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-  // tallenna napit omaan tilaansa
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
+const Statistics = ({good, neutral, bad}) => {
   return (
-    <div>
-      <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>
-        good
-      </button>
-      <button onClick={() => setNeutral(neutral + 1)}>
-        neutral
-      </button>
-      <button onClick={() => setBad(bad + 1)}>
-        bad
-      </button>
+    <>
       <h1>statistics</h1>
       <div>
         good {good}
@@ -38,6 +23,28 @@ const App = () => {
       <div>
         positive {((good) / (good + neutral + bad) * 100)}%
       </div>
+    </>
+  );
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>
+        good
+      </button>
+      <button onClick={() => setNeutral(neutral + 1)}>
+        neutral
+      </button>
+      <button onClick={() => setBad(bad + 1)}>
+        bad
+      </button>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
