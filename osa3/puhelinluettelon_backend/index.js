898e92e2bbcd4c1ed90e2c7bdbc567bab695d98d
ahnl <46541386+ahnl@users.noreply.github.com>
2020-11-29 18:17:47 +0200

    3.11

--- osa3/puhelinluettelon_backend/index.js
+++ osa3/puhelinluettelon_backend/index.js
@@ -16,6 +16,8 @@ morgan.token('post', function getId (req) {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post'));
 
+app.use(express.static('build'));
+
 let persons = [
     { 
       "name": "Arto Hellas", 
